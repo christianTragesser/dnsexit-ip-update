b811543d1e57ae25ee549e8ed65f90e205d6cd17
Christian Tragesser <christian.tragesser@asynchrony.com>
2020-06-23 23:10:21 -0500

    evaluate ip sync failing test

--- src/ip_update.py
+++ src/ip_update.py
@@ -1,8 +1,12 @@
 import requests
+import socket
 
-update_data_url = 'https://www.dnsexit.com/ipupdate/dyndata.txt'
 
 def get_update_url(data_url):
     update_data = requests.get(data_url)
     update_domain = update_data.text.split()[0].split('//')[-1]
     return 'https://'+update_domain
+
+
+def evaluate_ip_sync(domain):
+    return False
--- src/tests/test_update_url.py
+++ src/tests/test_update_url.py
@@ -3,7 +3,7 @@ import responses
 import mock
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ip_update import get_update_url
+from ip_update import get_update_url, evaluate_ip_sync
 
 update_data_url = 'https://test.local/ipupdate/dyndata.txt'
 update_data = '''
@@ -11,7 +11,6 @@ url=http://update.test.local/TestUpdate.sv
 base=http://update.test.local/
 version=2.0
 '''
-
 @responses.activate
 def test_get_update_url():
     #retrieves update data
@@ -21,3 +20,17 @@ def test_get_update_url():
 
     update_addr = get_update_url(update_data_url)
     assert update_addr == 'https://update.test.local/TestUpdate.sv'
+
+
+current_ip_resource = 'https://api.ipify.org'
+@responses.activate
+@mock.patch('socket.gethostbyname', return_value='2.2.2.2')
+def test_evaluate_ip_synced(mock_dns_lookup):
+    #lookup current egress IP address
+    #lookup current DNS A record for domain
+    #compare current IP with DNS IP
+    #return true if current, false if update is needed
+    responses.add(responses.GET, current_ip_resource, body='2.2.2.2')
+
+    sync = evaluate_ip_sync('test.local')
+    assert sync
