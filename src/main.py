15f58661e3bb8e392a252de1f61c92b1f64ac884
Christian Tragesser <christian.tragesser@asynchrony.com>
2020-06-24 15:14:07 -0500

    catch and exit for non-configured login, password, and domain variables

--- src/main.py
+++ src/main.py
@@ -4,18 +4,31 @@ import utils
 
 login = os.environ['LOGIN'] if 'LOGIN' in os.environ else ''
 password = os.environ['PASSWORD'] if 'PASSWORD' in os.environ else ''
-host = os.environ['HOST'] if 'HOST' in os.environ else ''
+domain = os.environ['DOMAIN'] if 'DOMAIN' in os.environ else ''
 interval = int(os.environ['CHECK_INTERVAL']) if 'CHECK_INTERVAL' in os.environ else 600
 update_data_url = os.environ['UPDATE_DATA_URL'] if 'UPDATE_DATA_URL' in os.environ else 'https://www.dnsexit.com/ipupdate/dyndata.txt'
+missing_acct_info = '''
+*********************************************************************
+dnsexit-ip-update requires DNSExit IP Update login, password, and
+domain values to run properly.  Currently, these values must be
+provided as environment variables "LOGIN", "PASSWORD, and "DOMAIN".
 
+Please refer to our github page for more documentation:
+https://github.com/christianTragesser/dnsexit-ip-update
+*********************************************************************
+'''
+
+if login == '' or password == '' or domain == '':
+    print(missing_acct_info)
+    exit(1)
 
 if __name__ == '__main__':
     print('Initializing...')
     update_fqdn = utils.get_update_url(update_data_url)
     while True:
-        if not utils.evaluate_ip_sync(host):
+        if not utils.evaluate_ip_sync(domain):
             try:
-                utils.update_dns_a_record(update_fqdn, login, password, host)
+                utils.update_dns_a_record(update_fqdn, login, password, domain)
             except Exception as e:
                 print(e)
                 exit(1)
