b610ca27559c9b1bada162857f45c45f5e086585
Christian Tragesser <christian@evoen.net>
2024-05-30 14:14:12 -0500

    correct logic for checking CLI parameters

--- main.go
+++ main.go
@@ -10,13 +10,16 @@ import (
 var version = "dev-build"
 
 func main() {
-	action := os.Args[1]
-
-	switch action {
-	case "version", "-v":
-		fmt.Printf("DNSExit dynamic IP client version: %s\n", version)
-
-	default:
+	if len(os.Args) < 2 {
 		dnsexit.CLI()
+	} else {
+		action := os.Args[1]
+		switch action {
+		case "version", "-v":
+			fmt.Printf("DNSExit dynamic IP client version: %s\n", version)
+
+		default:
+			dnsexit.CLI()
+		}
 	}
 }
